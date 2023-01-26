503044e7eaac78bb5d3b917313e1fffc4fe43abb
Ruthless <Ruthless@MSI>
2023-01-25 19:17:48 -0700

    Add project files.

---
+++ classesLabBrayden748211/MainProgram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesLabBrayden748211
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            Person ianBrooks = new(1, "Ian", "Brooks", "Red", 30, true);
+        
+            Person ginaJames = new(2, "Gina", "James", "Green", 18, false);
+
+            Person mikeBriscoe = new(3, "Mike", "Briscoe", "Blue", 45, true);
+
+            Person maryBeals = new(4, "Mary", "Beals", "Yellow", 28, true);
+
+            Console.WriteLine(ginaJames.personId + " " + ginaJames.firstName + " " + ginaJames.lastName + "'s favourite colour is " + ginaJames.favouriteColour);
+
+            Console.WriteLine(mikeBriscoe.ToString());
+
+            ianBrooks.favouriteColour = "White";
+            Console.WriteLine(ianBrooks.personId +":" + ianBrooks.firstName + " " + ianBrooks.lastName + "'s favourite colour is: " + ianBrooks.favouriteColour);
+            maryBeals.age = maryBeals.age + 10;
+            Console.WriteLine(maryBeals.firstName + " " + maryBeals.lastName + " age in 10 years is: " + maryBeals.age);
+        }
+    }
+}
---
+++ classesLabBrayden748211/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesLabBrayden748211
+{
+    class Person
+    {
+        // Fields
+        public int personId;
+        public string firstName;
+        public string lastName;
+        public string favouriteColour;
+        public int age;
+        public bool isWorking;
+
+        public Person(int argPersonId, string argFirstName, string argLastName, string argFavouriteColour, int argAge, bool argIsWorking) 
+        {
+            this.personId = argPersonId;
+            this.firstName = argFirstName;
+            this.lastName = argLastName;
+            this.favouriteColour = argFavouriteColour;
+            this.age = argAge;
+            this.isWorking = argIsWorking;
+        }
+        public override string ToString()
+        {
+            return string.Format("PersonId : {0} \nFirstName : {1} \nLastName : {2} \nFavoriteColour : {3} \nAge : {4} \nIsWorking : {5}", personId, firstName, lastName, favouriteColour, age, isWorking);
+        }
+    }
+}
